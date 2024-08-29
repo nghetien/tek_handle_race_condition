c716dc2f8e43b80d2fd53d285354d57ff800babf
tiennghe <tien.nq@teko.vn>
2024-08-29 13:00:43 +0700

    Update logic handle race condition.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,4 @@
+## 0.0.4
+* Update logic handle race condition.
 ## 0.0.3
 * Release first version.
--- lib/tek_handle_race_condition.dart
+++ lib/tek_handle_race_condition.dart
@@ -15,30 +15,30 @@ class TekHandleRaceCondition<T> {
     _completer = null;
   }
 
+  /// Nếu chưa khởi tạo thì khởi tạo Completer mới
+  /// Nếu đã khởi tạo rồi thì check isCompleted
+  /// Nếu isCompleted đã hoàn thành thì tạo Completer mới và chạy lại luồng cũ
+  /// Nếu isCompleted chưa hoàn thành thì trả về future cũ
   Future<T> call(TekHandleRaceConditionCallback<T> callback) async {
-    // Nếu chưa khởi tạo thì khởi tạo Completer mới
-    // Nếu đã khởi tạo rồi thì check isCompleted
-    // Nếu isCompleted đã hoàn thành thì tạo Completer mới và chạy lại luồng cũ
-    // Nếu isCompleted chưa hoàn thành thì trả về future cũ
-    if (_completer == null || _completer!.isCompleted) {
-      _completer = Completer<T>();
-      await _handleCallBack(callback);
-      return _completer!.future;
-    }
-
-    // Nếu chưa hoàn thành thì trả về future cũ
-    return _completer!.future;
+    if (_completer != null) return _completer!.future;
+    _completer = Completer<T>();
+    final result = await _handleCallBack(callback);
+    return result;
   }
 
   Future _handleCallBack(TekHandleRaceConditionCallback<T> callback) async {
+    late final dynamic result;
     try {
       // Call API
-      final result = await callback();
+      result = await callback();
       // When API call is done, complete the Completer with the result
       _completer!.complete(result);
     } catch (e) {
       // If there is an error, complete the Completer with the error
       _completer!.completeError(e);
+      result = null;
     }
+    reset();
+    return result;
   }
 }
